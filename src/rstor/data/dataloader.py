8241da7337e098a2502ed5ee4f36c564ce0012d5
Balthazar Neveu <balthazarneveu@gmail.com>
2024-02-25 01:50:02 +0100

    #1 add blur kernel  in dataloader

--- src/rstor/data/dataloader.py
+++ src/rstor/data/dataloader.py
@@ -2,6 +2,8 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 from typing import Tuple
 from rstor.synthetic_data.dead_leaves import dead_leaves_chart
+import cv2
+import random
 
 
 class DeadLeavesDataset(Dataset):
@@ -10,6 +12,7 @@ class DeadLeavesDataset(Dataset):
         size: Tuple[int, int] = (128, 128),
         length: int = 1000,
         frozen_seed: int = None,  # useful for validation set!
+        blur_kernel_half_size: int = [0, 2],
         **config_dead_leaves
         # number_of_circles: int = -1,
         # background_color: Optional[Tuple[float, float, float]] = (0.5, 0.5, 0.5),
@@ -18,25 +21,49 @@ class DeadLeavesDataset(Dataset):
         # radius_stddev: Optional[int] = -1,
     ):
         self.frozen_seed = frozen_seed
+
         self.size = size
         self.length = length
         self.config_dead_leaves = config_dead_leaves
+        self.blur_kernel_half_size = blur_kernel_half_size
+        if frozen_seed is not None:
+            random.seed(self.frozen_seed)
+            self.blur_kernel_half_size = [
+                (self.blur_kernel_half_size[0], self.blur_kernel_half_size[1]) for _ in range(length)]
 
     def __len__(self):
         return self.length
 
-    def __getitem__(self, idx: int) -> torch.Tensor:
-        # if torch.is_tensor(idx):
-        #     idx = idx.tolist()
+    def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor]:
         seed = self.frozen_seed + idx if self.frozen_seed is not None else None
         chart = dead_leaves_chart(self.size, seed=seed, **self.config_dead_leaves)
-        return torch.from_numpy(chart).permute(-1, 0, 1).float()
+        if self.frozen_seed is not None:
+            k_size_x, k_size_y = self.blur_kernel_half_size[idx]
+        else:
+            k_size_x = random.randint(self.blur_kernel_half_size[0], self.blur_kernel_half_size[1])
+            k_size_y = random.randint(self.blur_kernel_half_size[0], self.blur_kernel_half_size[1])
+        k_size_x = 2 * k_size_x + 1
+        k_size_y = 2 * k_size_y + 1
+        degraded_chart = cv2.GaussianBlur(chart, (k_size_x, k_size_y), 0)
+
+        def numpy_to_torch(ndarray):
+            return torch.from_numpy(ndarray).permute(-1, 0, 1).float()
+        return numpy_to_torch(degraded_chart), numpy_to_torch(chart)
 
 
 if __name__ == "__main__":
-    dead_leaves_dataset = DeadLeavesDataset()
+    dead_leaves_dataset = DeadLeavesDataset(colored=True)
     dead_leaves_dataloader = DataLoader(dead_leaves_dataset, batch_size=4, shuffle=True)
-    for i, batch in enumerate(dead_leaves_dataloader):
-        print(batch.shape)  # Should print [batch_size, size[0], size[1], 3] for each batch
+    for i, (batch_inp, batch_target) in enumerate(dead_leaves_dataloader):
+        print(batch_inp.shape, batch_target.shape)  # Should print [batch_size, size[0], size[1], 3] for each batch
         if i == 1:  # Just to break the loop after two batches for demonstration
+            import matplotlib.pyplot as plt
+            plt.subplot(1, 2, 1)
+            plt.imshow(batch_inp.permute(0, 2, 3, 1).reshape(-1, 128, 3).numpy())
+            plt.title("Degraded")
+            plt.subplot(1, 2, 2)
+            plt.imshow(batch_target.permute(0, 2, 3, 1).reshape(-1, 128, 3).numpy())
+            plt.title("Target")
+            plt.show()
+            print(batch_target)
             break
--- test/test_dataloader.py
+++ test/test_dataloader.py
@@ -25,6 +25,13 @@ def test_dead_leaves_dataset():
     }
 
     # Test case 3: Check item retrieval
-    item = dataset[0]
+    item, item_tgt = dataset[0]
     assert isinstance(item, torch.Tensor)
     assert item.shape == (3, 256, 256)
+
+    # Test case 4: Repeatable results with frozen seed
+    dataset1 = DeadLeavesDataset(frozen_seed=42)
+    dataset2 = DeadLeavesDataset(frozen_seed=42)
+    item1, item_tgt1 = dataset1[0]
+    item2, item_tgt2 = dataset2[0]
+    assert torch.all(torch.eq(item1, item2))
