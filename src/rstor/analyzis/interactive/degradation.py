cb090de571eb5904006e9a521cbfc6cc6f717d39
Balthazar Neveu <balthazarneveu@gmail.com>
2024-03-03 15:27:49 +0100

    #2 interactive = add noise

--- src/rstor/analyzis/interactive/degradation.py
+++ src/rstor/analyzis/interactive/degradation.py
@@ -5,7 +5,12 @@ import cv2
 
 @interactive(
     k_size_x=(1, [0, 10]),
-    k_size_y=(1, [0, 10])
+    k_size_y=(1, [0, 10]),
+    noise_stddev=(0., [0., 50.])
 )
-def degrade(chart: np.ndarray, k_size_x: int = 1, k_size_y: int = 1):
-    return cv2.GaussianBlur(chart, (2*k_size_x+1, 2*k_size_y+1), 0)
+def degrade(chart: np.ndarray, k_size_x: int = 1, k_size_y: int = 1, noise_stddev=0.):
+    blurred = cv2.GaussianBlur(chart, (2*k_size_x+1, 2*k_size_y+1), 0)
+    if noise_stddev > 0.:
+        noise = np.random.normal(0, noise_stddev/255., blurred.shape)
+        blurred += noise
+    return blurred
