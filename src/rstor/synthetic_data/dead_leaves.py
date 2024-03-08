2ac81ca723849defc4a41e2300708493bd7c6c19
Balthazar Neveu <balthazarneveu@gmail.com>
2024-03-08 23:25:05 +0100

    #19 fix docstring for cpu deadleaves

--- src/rstor/synthetic_data/dead_leaves.py
+++ src/rstor/synthetic_data/dead_leaves.py
@@ -1,4 +1,3 @@
-import random
 from typing import Tuple, Optional
 import numpy as np
 import cv2
@@ -15,7 +14,7 @@ def dead_leaves_chart(size: Tuple[int, int] = (100, 100),
                       radius_max: Optional[int] = -1,
                       radius_alpha: Optional[int] = 3,
                       seed: int = None,
-                      reverse=True) -> np.ndarray:
+                      reverse: Optional[bool] = True) -> np.ndarray:
     """
     Generation of a dead leaves chart by splatting circles on top of each other.
 
@@ -26,32 +25,39 @@ def dead_leaves_chart(size: Tuple[int, int] = (100, 100),
         background_color (Optional[Tuple[float, float, float]], optional):
         background color of the chart. Defaults to gray (0.5, 0.5, 0.5).
         colored (Optional[bool], optional): Whether to generate colored circles. Defaults to True.
-        radius_mean (Optional[int], optional): mean radius of the circles. Defaults to -1. (=> -2)
-        radius_stddev (Optional[int], optional): standard deviation of the radius of the circles.
+        radius_min (Optional[int], optional): minimum radius of the circles. Defaults to -1. (=> 1)
+        radius_max (Optional[int], optional): maximum radius of the circles. Defaults to -1. (=> 2000)
+        radius_alpha (Optional[int], optional): standard deviation of the radius of the circles.
         If negative, it is calculated based on the size. Defaults to -1.
         seed (int, optional): seed for the random number generator. Defaults to None
+        reverse: (Optional[bool], optional): View circles from the back view
+        by reversing order. Defaults to True.
+        WARNING: This option is extremely slow on GPU.
 
     Returns:
         np.ndarray: generated dead leaves chart as a NumPy array.
     """
     rng = np.random.default_rng(np.random.SeedSequence(seed))
-    
+
     if number_of_circles < 0:
         number_of_circles = 30 * max(size)
     if radius_min < 0.:
         radius_min = 1.
     if radius_max < 0.:
         radius_max = 2000.
-        
-    # Pick random circle centers and radia
+
+    # Pick random circle centers and radii
     center_x = rng.integers(0, size[1], size=number_of_circles)
     center_y = rng.integers(0, size[0], size=number_of_circles)
+
+    # Sample from a power law distribution for the p(radius=r) = (r.clip(radius_min, radius_max))^(-alpha)
 
     radius = rng.uniform(
         low=radius_max ** (1 - radius_alpha),
         high=radius_min ** (1 - radius_alpha),
         size=number_of_circles
-        )
+    )
+    # Using the change of variables formula for random variables.
     radius = radius ** (-1/(radius_alpha - 1))
     radius = radius.round().astype(int)
 
@@ -61,8 +67,7 @@ def dead_leaves_chart(size: Tuple[int, int] = (100, 100),
     else:
         color = rng.uniform(0.25, 0.75, size=(number_of_circles, 1))
         color = np.concatenate((color, color, color), axis=1)
-        
-    
+
     if reverse:
         chart = np.zeros((size[0], size[1], 3), dtype=np.float32)
         buffer = np.zeros_like(chart)
@@ -71,10 +76,10 @@ def dead_leaves_chart(size: Tuple[int, int] = (100, 100),
             cv2.circle(buffer, (center_x[i], center_y[i]), radius[i], color[i], -1)
             chart += buffer * is_not_covered_mask
             is_not_covered_mask = cv2.circle(is_not_covered_mask, (center_x[i], center_y[i]), radius[i], 0, -1)
-            
+
             if not np.any(is_not_covered_mask):
-                break 
-                
+                break
+
         chart += np.multiply(background_color, np.ones((size[0], size[1], 3), dtype=np.float32)) * is_not_covered_mask
     else:
         chart = np.multiply(background_color, np.ones((size[0], size[1], 3), dtype=np.float32))
@@ -82,7 +87,6 @@ def dead_leaves_chart(size: Tuple[int, int] = (100, 100),
             # circle is inplace
             cv2.circle(chart, (center_x[i], center_y[i]), radius[i], color[i], -1)
 
-        
     chart = chart.clip(0, 1)
 
     if not colored:
