c26871e14ef33dd49a56084a41921e03b75a50bd
Jamy Lafenetre <46826148+Jamy-L@users.noreply.github.com>
2024-03-08 21:35:47 +0100

    Update unitary tests

--- test/test_dataloader.py
+++ test/test_dataloader.py
@@ -31,20 +31,20 @@ def test_dead_leaves_dataset():
     assert item.shape == (3, 256, 256)
 
     # Test case 4: Repeatable results with frozen seed
-    dataset1 = DeadLeavesDataset(frozen_seed=42, noise_stddev=(0, 0))
-    dataset2 = DeadLeavesDataset(frozen_seed=42, noise_stddev=(0, 0))
+    dataset1 = DeadLeavesDataset(frozen_seed=42, noise_stddev=(0, 0), number_of_circles=256)
+    dataset2 = DeadLeavesDataset(frozen_seed=42, noise_stddev=(0, 0), number_of_circles=256)
     item1, item_tgt1 = dataset1[0]
     item2, item_tgt2 = dataset2[0]
     assert torch.all(torch.eq(item1, item2))
     
     # Test case 5: Visualize
-    dataset = DeadLeavesDataset(size=(256, 256), length=500, frozen_seed=42, number_of_circles=5_000,
-                                   background_color=(0.2, 0.4, 0.6), colored=True, radius_min=1, radius_alpha=3,
-                                   noise_stddev=(0, 0), ds_factor=1)
-    item, item_tgt = dataset[0]
-    import matplotlib.pyplot as plt
-    plt.figure()
-    plt.imshow(item.permute(1, 2, 0).detach().cpu())
-    plt.show()
-    print("done")
+    # dataset = DeadLeavesDataset(size=(256, 256), length=500, frozen_seed=43,
+    #                                 background_color=(0.2, 0.4, 0.6), colored=True, radius_min=1, radius_alpha=3,
+    #                                 noise_stddev=(0, 0), ds_factor=1)
+    # item, item_tgt = dataset[0]
+    # import matplotlib.pyplot as plt
+    # plt.figure()
+    # plt.imshow(item.permute(1, 2, 0).detach().cpu())
+    # plt.show()
+    # print("done")
 
--- test/test_gpu_vs_cpu_dataloader.py
+++ test/test_gpu_vs_cpu_dataloader.py
@@ -13,16 +13,30 @@ def test_gpu_vs_cpu_dataloader():
     if not numba.cuda.is_available():
         return
 
-    n = 20
+    n = 10
     print("\n")
 
-    dataset = DeadLeavesDatasetGPU()
+    print("=== Dead leaves with reversing")
+    dataset = DeadLeavesDatasetGPU(number_of_circles=256, reverse=True)
     t1 = perf_counter()
     for i in range(n):
         _ = dataset[i]
     print(f"Mean time on {n} samples (numba) : {(perf_counter()-t1)/n}")
 
-    dataset = DeadLeavesDataset()
+    dataset = DeadLeavesDataset(number_of_circles=256, reverse=True)
+    t1 = perf_counter()
+    for i in range(n):
+        _ = dataset[i]
+    print(f"Mean time on {n} samples (cv2): {(perf_counter()-t1)/n}")
+
+    print("=== Dead leaves without reversing")
+    dataset = DeadLeavesDatasetGPU(number_of_circles=256, reverse=False)
+    t1 = perf_counter()
+    for i in range(n):
+        _ = dataset[i]
+    print(f"Mean time on {n} samples (numba) : {(perf_counter()-t1)/n}")
+
+    dataset = DeadLeavesDataset(number_of_circles=256, reverse=False)
     t1 = perf_counter()
     for i in range(n):
         _ = dataset[i]
