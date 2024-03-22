18dbd686b2eb4bfa77e5c9fed3531b448480855d
Balthazar Neveu <balthazarneveu@gmail.com>
2024-03-22 23:12:19 +0100

    #9 add SSIM from torch metrics

--- src/rstor/learning/metrics.py
+++ src/rstor/learning/metrics.py
@@ -1,5 +1,6 @@
 import torch
-from rstor.properties import METRIC_PSNR, REDUCTION_AVERAGE, REDUCTION_SKIP
+from rstor.properties import METRIC_PSNR, METRIC_SSIM, REDUCTION_AVERAGE, REDUCTION_SKIP
+from torchmetrics import StructuralSimilarityIndexMeasure as SSIM
 
 
 def compute_psnr(
@@ -30,6 +31,26 @@ def compute_psnr(
     return average_psnr
 
 
+def compute_ssim(
+    predic: torch.Tensor,
+    target: torch.Tensor,
+    reduction=REDUCTION_AVERAGE
+) -> torch.Tensor:
+    """
+    Compute the average SSIM metric for a batch of predicted and true values.
+
+    Args:
+        predic (torch.Tensor): [N, C, H, W] predicted values.
+        target (torch.Tensor): [N, C, H, W] target values.
+
+    Returns:
+        torch.Tensor: The average SSIM value for the batch.
+    """
+    ssim = SSIM(data_range=1.0, reduction=None if reduction == REDUCTION_SKIP else "elementwise_mean")
+    ssim_value = ssim(predic, target)
+    return ssim_value
+
+
 def compute_metrics(predic: torch.Tensor, target: torch.Tensor, reduction=REDUCTION_AVERAGE) -> dict:
     """
     Compute the metrics for a batch of predicted and true values.
@@ -42,5 +63,10 @@ def compute_metrics(predic: torch.Tensor, target: torch.Tensor, reduction=REDUCT
         dict: computed metrics.
     """
     average_psnr = compute_psnr(predic, target, reduction=reduction)
-    metrics = {METRIC_PSNR: average_psnr.item() if reduction != REDUCTION_SKIP else average_psnr}
+    metrics = {
+        METRIC_PSNR: average_psnr.item() if reduction != REDUCTION_SKIP else average_psnr
+    }
+
+    ssim_value = compute_ssim(predic, target, reduction=reduction)
+    metrics[METRIC_SSIM] = ssim_value.item() if reduction != REDUCTION_SKIP else ssim_value
     return metrics
