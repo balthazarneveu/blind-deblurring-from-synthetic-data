1c9822dfaa762a84d020f2b78cc565c4d74c57f4
Balthazar Neveu <balthazarneveu@gmail.com>
2024-03-22 23:56:37 +0100

    #26 natural images interactive render shows metrics

--- src/rstor/analyzis/interactive/metrics.py
+++ src/rstor/analyzis/interactive/metrics.py
@@ -3,7 +3,7 @@ import torch
 import numpy as np
 
 
-def get_metrics(prediction: torch.Tensor, target: torch.Tensor, global_params: dict = {}):
+def get_metrics(prediction: torch.Tensor, target: torch.Tensor, image_name, global_params: dict = {}):
     if isinstance(prediction, np.ndarray):
         prediction_ = torch.from_numpy(prediction).permute(-1, 0, 1).float().unsqueeze(0)
     else:
@@ -12,8 +12,8 @@ def get_metrics(prediction: torch.Tensor, target: torch.Tensor, global_params: d
         target_ = torch.from_numpy(target).permute(-1, 0, 1).float().unsqueeze(0)
     else:
         target_ = target
-    print(prediction_.shape, target_.shape)
     metrics = compute_metrics(prediction_, target_)
     global_params["metrics"] = metrics
-    title = " ".join([f"{key}: {value:.4f}" for key, value in metrics.items()])
-    global_params["__output_styles"]["restored"] = {"title": title}
+    title = f"{image_name}: "
+    title += " ".join([f"{key}: {value:.4f}" for key, value in metrics.items()])
+    global_params["__output_styles"][image_name] = {"title": title, "image_name": image_name}
