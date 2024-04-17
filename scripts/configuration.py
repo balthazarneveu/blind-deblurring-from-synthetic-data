8398febe105e03e6b8b990ee447718f3238164f0
Balthazar Neveu <balthazarneveu@gmail.com>
2024-04-17 15:12:20 +0200

    reboot for perceptual_denoiser

--- scripts/configuration.py
+++ scripts/configuration.py
@@ -5,12 +5,12 @@ GIT_USER = "balthazarneveu"  # Your git user name
 GIT_REPO = "blind-deblurring-from-synthetic-data"  # Your current git repo
 # Keep free unless you need to acess kaggle datasets. You'll need to modify the remote_training_template.ipynb.
 KAGGLE_DATASET_LIST = [
-    "balthazarneveu/deadleaves-div2k-512", # Deadleaves classic
-    "balthazarneveu/deadleaves-primitives-div2k-512", # Deadleaves with extra primitives
-    "balthazarneveu/motion-blur-kernels", # Motion blur kernels
+    "balthazarneveu/deadleaves-div2k-512",  # Deadleaves classic
+    "balthazarneveu/deadleaves-primitives-div2k-512",  # Deadleaves with extra primitives
+    "balthazarneveu/motion-blur-kernels",  # Motion blur kernels
     "joe1995/div2k-dataset",
 ]
-WANDBSPACE = "deblur-from-deadleaves"
+WANDBSPACE = "perceptual-denoiser"
 TRAIN_SCRIPT = "scripts/train.py"  # Location of the training script
 
 ROOT_DIR = Path(__file__).parent
